7675520c6b33d84ea9a076324f59f5ff15dbf57d
Kafui Beryl Obiatey <kbobiatey@myfidelitybank.net>
2023-09-27 12:01:03 +0000

    Dtarted DAl entry

---
+++ DAL/Member.cs
@@ -0,0 +1,25 @@
+﻿namespace ChurchMI.DAL
+{
+    public partial class Member
+    {
+        public Guid MemberId { get; set; }
+        public Guid RoledId { get; set; }
+        public Guid TitleId { get; set; }
+        public Guid GenderId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string? OtherName { get; set; }
+        public string LastName { get; set; } = null!;
+        public DateOnly? DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; } = null!;
+        public string EmergencyContactName { get; set; } = null!;
+        public string EmergencyContactNumber { get; set; } = null!;
+        public string EmergencyContactRelationship { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string YearJoined { get; set; } = null!;
+        public string Hometown { get; set; } = null!;
+        public string Nationality { get; set; } = null!;
+        public string ResidentialAddress { get; set; } = null!;
+        public Guid MaritalStatusId { get; set; }
+        
+    }
+}
